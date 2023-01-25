d03c61406b368b24f5e4a4f9d6b9fb7f4254fc2d
sehrschlechtYT <66412605+sehrschlechtYT@users.noreply.github.com>
2023-01-25 16:18:33 +0100

    add singleton to main

--- src/main/java/yt/sehrschlecht/plugintemplate/SpigotPlugin.java
+++ src/main/java/yt/sehrschlecht/plugintemplate/SpigotPlugin.java
@@ -3,15 +3,22 @@ package yt.sehrschlecht.plugintemplate;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SpigotPlugin extends JavaPlugin {
+    private static SpigotPlugin instance;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        instance = this;
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+
+    }
+
+    /**
+     * @return An instance of the plugin, if it is enabled.
+     */
+    public static SpigotPlugin getInstance() {
+        return instance;
     }
 }
